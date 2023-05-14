26b85e73f6cc6f141f78faf0432fb1ff986b4b24
hover <1178966714@qq.com>
2023-05-14 22:18:14 +0800

    test2

--- untitled/src/main/java/org/example/git.java
+++ untitled/src/main/java/org/example/git.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class git {
     public static void main(String[] args) {
-        System.out.println("hello git !");
+        System.out.println("hello git1 !");
+        System.out.println("hello git2 !");
     }
 }
