51d0edc22785d510f1621032b94fb580a77ba55b
hover <1178966714@qq.com>
2023-05-14 22:16:33 +0800

    test1

---
+++ untitled/src/main/java/org/example/App.java
@@ -0,0 +1,23 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static int sum(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static int divide(int a, int b)
+    {
+        return a - b;
+    }
+}
